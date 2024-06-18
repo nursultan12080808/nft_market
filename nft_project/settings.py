aa5ce822f5a868b52aeef6dd25fb74e9657f7690
nursultan12080808 <nursultan.top.game@gmail.com>
2024-06-18 15:54:54 +0600

    last commit

--- nft_project/settings.py
+++ nft_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h&7-ywqgyuat^-3qby%*_5e3_+nv0nhygbf2s9zgfiwoqjfrq3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = '*'
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
